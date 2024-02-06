2aabf30f5c2f3ed0316eb265138e8a5a2b94d43a
eijjeka <eijjeka@gmail.com>
2024-02-06 18:07:41 +0200

    about done

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,11 @@
 import styles from "./page.module.scss";
-import { Services, Hero, Recenzie } from "./components";
+import { Services, Hero, About, Recenzie } from "./components";
 
 export default function Home() {
     return (
         <main className={styles.main}>
             <Hero />
+            <About />
             <Services />
             <Recenzie />
         </main>
