f52ec41ae74cf5a9044ea0dc64072b3e4413d46b
Saniokkk <kashirin.alexsandr91@gmail.com>
2024-02-08 02:01:56 +0200

    hero section done without bg

---
+++ app/projects/[id]/components/Hero/Hero.jsx
@@ -0,0 +1,40 @@
+import { Container, Title } from "@/shared";
+import cn from "classnames";
+import Image from "next/image";
+import { instrumentSerif } from "@/app/fonts";
+
+import s from "./Hero.module.scss";
+
+export const Hero = ({
+  heroData: { mainTitle, year, urlImage, projectType, descriptionAboutCompony },
+}) => {
+  return (
+    <section className={s.section}>
+      <Container>
+        <div className={s.topSideWrapper}>
+          <Title
+            tag="h1"
+            className={s.mainTitle}
+          >
+            {mainTitle}
+          </Title>
+          <p className={cn(instrumentSerif.className, s.projectType)}>
+            <span className={s.year}>{year}</span>
+            {projectType}
+          </p>
+        </div>
+        <Image
+          className={s.image}
+          src={urlImage}
+          alt="Preview project"
+          width={1248}
+          height={750}
+        />
+        <div className={s.wrapAbout}>
+          <p className={s.titleAbout}>About the company</p>
+          <p className={s.textAbout}>{descriptionAboutCompony}</p>
+        </div>
+      </Container>
+    </section>
+  );
+};
--- app/projects/[id]/page.jsx
+++ app/projects/[id]/page.jsx
@@ -1,3 +1,22 @@
-export default function Page({ params }) {
-  return <>My Post: {params.id}</>;
+import { Hero } from "./components";
+// import heroImgMob from "@/public/projects/hero-mob.jpg";
+// import heroImgTab from "@/public/projects/hero-tablet.jpg";
+import urlImage from "@/public/projects/hero-desk.jpg";
+
+const heroData = {
+  mainTitle: "SMME",
+  year: 2023,
+  projectType: "UI/UX Design, Multipage site",
+  urlImage,
+  descriptionAboutCompony:
+    "Founded in 2020, Social Media Marketing Experts was born out of the vision of a team of dedicated digital enthusiasts who recognized the transformative power of strategic marketing in the digital age. From humble beginnings, they have grown into a dynamic agency that provides cutting-edge solutions for businesses of all sizes. The mission of Social Media Marketing Experts is simple but profound - to help companies not just navigate but dominate the digital world. ",
+};
+
+export default function ProjectPage({ params }) {
+  return (
+    <>
+      My Post: {params.id}
+      <Hero heroData={heroData}></Hero>
+    </>
+  );
 }
