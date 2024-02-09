eda4a44d3508b42a3aa99bcaea3537f02fd8b210
Saniokkk <kashirin.alexsandr91@gmail.com>
2024-02-10 01:59:02 +0200

    projects section done

--- app/projects/[id]/components/OtherProjects/OtherProjects.jsx
+++ app/projects/[id]/components/OtherProjects/OtherProjects.jsx
@@ -8,35 +8,9 @@ import { instrumentSans } from "@/app/fonts";
 import { CardProject } from "./components/CardProject";
 import { Container, Title, Text } from "@/shared";
 import OtherProjectsSwiper from "./components/OtherProjectsSwiper";
+import { projectData } from "@/data/projectsPage";
 
-const data = [
-  {
-    id: 1,
-    srcImg: smmImg,
-    name: "SMME",
-    description: "UI/UX Design, Multipage site",
-  },
-  {
-    id: 2,
-    srcImg: techlightImg,
-    name: "Techlight ",
-    description: "UI/UX Design, E-commerce",
-  },
-  {
-    id: 3,
-    srcImg: a2b_haulImg,
-    name: "A2b haul",
-    description: "UI/UX Design, Multipage site",
-  },
-  {
-    id: 4,
-    srcImg: kvitkarniaImg,
-    name: "Kvitkarnia",
-    description: "UI/UX Design, Multipage site",
-  },
-];
-
-export const OtherProjects = () => {
+export const OtherProjects = ({ data }) => {
   return (
     <section className={s.section}>
       <Container className={s.container}>
@@ -44,10 +18,10 @@ export const OtherProjects = () => {
           className={cn(instrumentSans.className, s.title)}
           tag="h2"
         >
-         <span className={s.innerTitle}>Other</span> projects
+          <span className={s.innerTitle}>Other</span> projects
         </Title>
       </Container>
-        <OtherProjectsSwiper data={data} />
+      <OtherProjectsSwiper data={data} />
     </section>
   );
 };
--- app/projects/[id]/page.jsx
+++ app/projects/[id]/page.jsx
@@ -1,23 +1,17 @@
+import { projectData } from "@/data/projectsPage";
 import { Hero, OtherProjects } from "./components";
-// import heroImgMob from "@/public/projects/hero-mob.jpg";
-// import heroImgTab from "@/public/projects/hero-tablet.jpg";
-import urlImage from "@/public/projects/hero-desk.jpg";
-
-const heroData = {
-  mainTitle: "SMME",
-  year: 2023,
-  projectType: "UI/UX Design, Multipage site",
-  urlImage,
-  descriptionAboutCompony:
-    "Founded in 2020, Social Media Marketing Experts was born out of the vision of a team of dedicated digital enthusiasts who recognized the transformative power of strategic marketing in the digital age. From humble beginnings, they have grown into a dynamic agency that provides cutting-edge solutions for businesses of all sizes. The mission of Social Media Marketing Experts is simple but profound - to help companies not just navigate but dominate the digital world. ",
-};
 
 export default function ProjectPage({ params }) {
+  const { hero, projects } = projectData;
+  const otherProjects = projects.filter(
+    (project) => project.id !== Number(params.id)
+  );
+
   return (
     <>
       My Post: {params.id}
-      <Hero heroData={heroData}></Hero>
-      <OtherProjects />
+      <Hero heroData={hero}></Hero>
+      <OtherProjects data={otherProjects} />
     </>
   );
 }
